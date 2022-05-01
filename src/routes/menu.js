40be854157810ce36313b8de942911837965396d
Pipitpong <pipitpong.pi@gmail.com>
2022-05-01 21:19:55 +0700

    deploy

--- src/routes/menu.js
+++ src/routes/menu.js
@@ -4,10 +4,10 @@ const menuController = require("../controllers/menu.controller");
 const router = express.Router();
 
 router.get("/menus", menuController.getAllMenu);
-router.get("/menus/:name", menuController.getDetailMenu);  
-router.get("/menus/recommend", menuController.getRecommendMenu)
+router.get("/menus/recommend", menuController.getRecommendMenu);
+router.get("/menus/:name", menuController.getDetailMenu);
 router.post("/menus", menuController.addMenu);
 router.put("/menus/:id", menuController.updateMenu);
 router.delete("/menus/:id", menuController.deleteMenu);
 
-module.exports = router ;
+module.exports = router;
